54c81a7f2d1e8dbb21eec781ad5b005afcf38542
margonaut <margaret.morgane@gmail.com>
2015-09-23 17:09:56 -0400

    Installed Casein and Casein Projects

--- Gemfile
+++ Gemfile
@@ -1,5 +1,8 @@
 source 'https://rubygems.org'
 
+gem 'casein', '~>5.1.1'
+
+gem 'bootstrap-wysihtml5-rails'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.2.4'
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,6 +13,8 @@
 //= require jquery
 //= require jquery_ujs
 //= require foundation
+//= require bootstrap-wysihtml5
 //= require_tree .
+//= require bootstrap-wysihtml5
 
 $(function(){ $(document).foundation(); });
